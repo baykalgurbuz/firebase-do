8033150b0c70c8c571fe14dc474fe15b586ab166
ebaykalgurbuz <ebaykalgurbuz@gmail.com>
2023-05-19 17:43:03 +0300

    Readed the data in firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
  
 import './App.css';
-import React ,{useState}from 'react';
+import React ,{useState,useEffect}from 'react';
 import {AiOutlinePlus} from 'react-icons/ai';
 import Todo from './Todo';
+import { collection, onSnapshot, query } from 'firebase/firestore';
+import {db} from './firebase'
+ 
+
 const style={
   bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1cB5E0]`,
   container:`bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
@@ -14,7 +18,21 @@ const style={
 
 }
 function App() {
-  const [todos,setTodos]=useState(['Learn React','Grind Leetcode'])
+  const [todos,setTodos]=useState([])
+  
+  useEffect(()=>{
+    const q =query(collection(db,'todos'))
+    const unsubscribe=onSnapshot(q,(querySnapshot)=>{
+      let todosArr=[]
+      querySnapshot.forEach((doc)=>{
+        todosArr.push({...doc.data(),id:doc.id})
+      });
+      setTodos(todosArr)
+    })
+    return ()=>unsubscribe()
+  },[])
+  
+  
   return (
     <div className={style.bg}>
      <div className={style.container}>
